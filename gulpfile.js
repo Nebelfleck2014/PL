dfff589e90b6229662dfa27fa5b10c3511ee1232
Talgat <duzbatyrovtalgat96@gmail.com>
2019-01-07 11:02:45 +0400

    собрал проект

---
+++ gulpfile.js
@@ -0,0 +1,110 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const watch = require('gulp-watch');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglifyjs');
+const cssnano = require('gulp-cssnano');
+const rename = require('gulp-rename');
+const del = require('del');
+const pngquant = require('imagemin-pngquant');
+const cache = require('gulp-cache');
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function (done) {
+  return gulp.src('src/sass/**/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.reload({stream: true}))
+    done();
+});
+
+gulp.task('browser-sync', function(done) { // Создаем таск browser-sync
+    browserSync({ // Выполняем browser Sync
+        server: { // Определяем параметры сервера
+            baseDir: 'src' // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
+    });
+    done();
+});
+
+gulp.task('scripts', function(done) {
+    return gulp.src([ // Берем все необходимые библиотеки
+        'src/libs/jquery/dist/jquery.min.js', // Берем jQuery
+        'src/libs/magnific-popup/dist/jquery.magnific-popup.min.js' // Берем Magnific Popup
+        ])
+        .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
+        .pipe(uglify()) // Сжимаем JS файл
+        .pipe(gulp.dest('src/js')); // Выгружаем в папку app/js
+        done();
+})
+
+gulp.task('css-libs', gulp.parallel('sass', async function(done) {
+    return gulp.src('src/css/libs.css') // Выбираем файл для минификации
+        .pipe(cssnano()) // Сжимаем
+        .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
+        .pipe(gulp.dest('src/css')); // Выгружаем в папку app/css
+        done();
+}));
+
+
+gulp.task('watch', function(done) {
+    gulp.watch('src/sass/**/*.sass', gulp.parallel('sass')); // Наблюдение за sass файлами в папке sass
+    gulp.watch('src/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+    gulp.watch('src/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
+    done();
+});
+
+
+gulp.task('clean', function() {
+    return del.sync('build'); // Удаляем папку build перед сборкой
+});
+
+gulp.task('img', function() {
+    return gulp.src('src/img/**/*') // Берем все изображения из app
+        .pipe(imagemin({ // Сжимаем их с наилучшими настройками
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('build/img')); // Выгружаем на продакшен
+});
+
+gulp.task('img', function() {
+    return gulp.src('src/img/**/*') // Берем все изображения из app
+        .pipe(cache(imagemin({  // Сжимаем их с наилучшими настройками с учетом кеширования
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        })))
+        .pipe(gulp.dest('build/img')); // Выгружаем на продакшен
+});
+
+gulp.task('build', gulp.parallel('sass', 'scripts', function(done) {
+
+    var buildCss = gulp.src([ // Переносим CSS стили в продакшен
+        'src/css/main.css',
+        'src/css/libs.min.css'
+        ])
+    .pipe(gulp.dest('build/css'))
+
+    var buildFonts = gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
+    .pipe(gulp.dest('build/fonts'))
+
+    var buildJs = gulp.src('src/js/**/*') // Переносим скрипты в продакшен
+    .pipe(gulp.dest('build/js'))
+
+    var buildHtml = gulp.src('src/*.html') // Переносим HTML в продакшен
+    .pipe(gulp.dest('build'));
+    done();
+}));
+
+gulp.task('clear', function () {
+    return cache.clearAll();
+})
+
+gulp.task('default', gulp.parallel('watch', 'browser-sync', 'css-libs', 'scripts'));
